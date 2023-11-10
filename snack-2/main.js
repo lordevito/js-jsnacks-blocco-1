42f8b9219fb8a9d5debb61745285401bdec3a171
lordevito <lollo.devito@hotmail.com>
2023-11-10 11:35:35 +0100

    fix: bugs

--- snack-2/main.js
+++ snack-2/main.js
@@ -2,11 +2,19 @@
 
 /*Creiamo due prompt per chiedere le due parole da conforntare*/
 
-let primaParola = prompt(`Inserisci la prima parola`);
-let secondaParola = prompt(`Inserisci la prima parola`);
+const primaParola = prompt(`Inserisci la prima parola`);
+const secondaParola = prompt(`Inserisci la prima parola`);
 
 /*Creiamo la condizione in cui stampiamo in console la parola inserita più grande*/
 
 if (primaParola.length > secondaParola.length) {
-    console.log (``)
+    console.log (`La parola più corta è: ` + secondaParola);
+    console.log (`La parola più lunga è: ` + primaParola);
+}
+else if (secondaParola.length > primaParola.length) {
+    console.log (`La parola più corta è: ` + primaParola);
+    console.log (`La parola più lunga è: ` + secondaParola);
+}
+else {
+    console.log(`Le parole hanno la stessa lunghezza`);
 }
