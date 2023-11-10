bae4447037a09fb20a46f324f0992dbf4f797cba
lordevito <lollo.devito@hotmail.com>
2023-11-10 11:28:41 +0100

    first commit

---
+++ snack-2/main.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+/*Creiamo due prompt per chiedere le due parole da conforntare*/
+
+let primaParola = prompt(`Inserisci la prima parola`);
+let secondaParola = prompt(`Inserisci la prima parola`);
+
+/*Creiamo la condizione in cui stampiamo in console la parola inserita più grande*/
+
+if (primaParola.length > secondaParola.length) {
+    console.log (``)
+}
