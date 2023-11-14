e1d94d3a5c1c8752da533203ba2be9c3aa4e3747
lordevito <lollo.devito@hotmail.com>
2023-11-14 11:04:31 +0100

    feat: snack 4

---
+++ main.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+const array = [];
+
+const somma = Number(`50`);
+let i = 0;
+
+
+while () {
+
+    let numb = Number(prompt(`Inserisci un numero`));
+    while(isNaN(numb)){
+        let numb = Number(prompt(`Inserisci un altro numero`));
+    }
+
+}
